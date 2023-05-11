f80a18805db6b2d50f3a5cf8ea567ffe0990323a
saikiran <saikiran.davangams@gmail.com>
2023-05-11 23:19:12 +0200

    updated

--- src/components/Data_ingestion.py
+++ src/components/Data_ingestion.py
@@ -47,7 +47,7 @@ class DataIngestion:
 
 if __name__ == "__main__" :
     obj = DataIngestion()
-    train_data , test_data = obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
     data_transformation.initiate_data_transformation(train_data,test_data)
